251c0ee89c85b408e6316b4b70dfa3352074d000
Ubirajara Marsicano <ubirajara_neto@hotmail.com>
2018-07-11 17:21:16 -0300

    html and css of first page completely implemented

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -26,34 +26,41 @@
           <button class="btn btn-primary regBtn" type="button" style="border-radius:8px;padding-top:20px;padding-right:32px;padding-bottom:18px;padding-left:32px;margin:5%;background-color:#f9a825;border-color:#f9a825;"><a href="login.html" style="color: white;text-decoration: none;"><i class="fa fa-user-plus"></i> REGISTER</a></button>
         </div>
       </section>
-      <section style="background-color:#fafafa;">
-        <div style="text-align:center;padding-top:10px;padding-bottom:10px;"><span style="font-style:italic;color:#a2a6af;">As featured in...</span></div><div class="customer-logos" style="background-color:#fafafa;padding-top:2%;padding-bottom:5%;">
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/aol.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bing.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bloomberg-businessweek.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bloomberg-tv.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bloomberg-view.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bloomberg.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/bswann.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/business-report.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/cnbc.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/drdave.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/fast-markets.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/fox.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/google.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/ibt.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/jaytaylor.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/market-watch.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/msn.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/reuters.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/sina.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/thestreet.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/usnews.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/wrko.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/wsj.png"></div>
-        <div class="slide"><img class="imgSize" src="../assets/img/Logos/yahoo.png"></div>
+      <div style="text-align:center;padding-top:10px;padding-bottom:10px;background-color:#fafafa;">
+        <span style="font-style:italic;color:#a2a6af;">As featured in...</span>
       </div>
-      </section>
+      <carousel
+          style="background-color:#fafafa;padding-top:2%;padding-bottom:5%;"
+          autoplay
+          :per-page="5"
+          :pagination-enabled="false"
+          :scroll-per-page="false"
+          :loop="true">
+        <slide><img class="imgSize" src="../assets/img/Logos/aol.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bing.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bloomberg-businessweek.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bloomberg-tv.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bloomberg-view.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bloomberg.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/bswann.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/business-report.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/cnbc.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/drdave.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/fast-markets.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/fox.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/google.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/ibt.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/jaytaylor.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/market-watch.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/msn.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/reuters.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/sina.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/thestreet.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/usnews.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/wrko.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/wsj.png"></slide>
+        <slide><img class="imgSize" src="../assets/img/Logos/yahoo.png"></slide>
+      </carousel>
       <div style="background-color:#fafafa;">
         <div class="container">
           <div class="row">
@@ -63,12 +70,12 @@
           </div>
           <div class="row" style="margin-top: 3%;">
             <div class="col-md-6">
-              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="assets/img/AV-logo-color.png" style="width:50%;margin:auto;">
+              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../assets/img/AV-logo-color.png" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">AnthemVault</h4>
                 <p>Buy, store, sell and gift metals. Gold and Silver ownership made easy, secure and affordable<br></p><a class="btn btn-primary" role="button" href="https://www.anthemvault.com/" target="_blank">Visit AnthemVault</a></div>
             </div>
             <div class="col-md-6">
-              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height:300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="assets/img/amagi-metals-logo.gif" style="width:50%;margin:auto;">
+              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height:300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../assets/img/amagi-metals-logo.gif" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">Amagi Metals</h4>
                 <p>Amagi Metals is a global bullion dealer that serves the world with quality precious metals.<br></p><a class="btn btn-primary" role="button" href="https://www.amagimetals.com/" target="_blank">Visit Amagi Metals</a></div>
             </div>
@@ -77,12 +84,12 @@
         <div class="container">
           <div class="row">
             <div class="col-md-6">
-              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="assets/img/Anthem-gold-1.png" style="width:50%;margin:auto;">
+              <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../assets/img/Anthem-gold-1.png" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">Anthem Gold</h4>
                 <p>Anthem Gold is a global gold dealer that serves the world with quality gold bars that can be obtained with crypto currencies.<br></p><a class="btn btn-primary" role="button" href="https://www.anthemgold.com/" target="_blank">Visit Anthem Gold</a></div>
             </div>
             <div class="col-md-6">
-              <div class="senka" style="margin-bottom:2%;margin-top:5%;margin-left:0;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="assets/img/AV-Pro-logo-light.png" style="width:50%;margin:auto;">
+              <div class="senka" style="margin-bottom:2%;margin-top:5%;margin-left:0;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="../assets/img/AV-Pro-logo-light.png" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">AnthemVault Pro</h4>
                 <p>Anthem Vault Professional allows investment advisor portfolios to shine with physical Gold &amp; Silver bullion ownership<br></p><a class="btn btn-primary" role="button" href="http://www.anthemvaultprofessional.com/" target="_blank">Visit AnthemVault Pro</a>
               </div>
@@ -94,12 +101,12 @@
         <div class="container">
           <div class="row">
             <div class="col-md-6">
-              <div class="senka" style="margin-right:0;margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="assets/img/ANR-logo-web-light.png" style="width:50%;margin:auto;">
+              <div class="senka" style="margin-right:0;margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="../assets/img/ANR-logo-web-light.png" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">Anthem National Reserve</h4>
                 <p>Expert consultation services in the acquisition of bullion precious metals and rare coin investments.<br></p><a class="btn btn-primary" role="button" href="http://www.anthemnationalreserve.com/" target="_blank">Visit ANR</a></div>
             </div>
             <div class="col-md-6">
-              <div class="senka" style="margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;margin-bottom: 2%;"><img class="img-fluid" src="assets/img/anthem-bunker-logo-light.png" style="width:50%;margin:auto;">
+              <div class="senka" style="margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;margin-bottom: 2%;"><img class="img-fluid" src="../assets/img/anthem-bunker-logo-light.png" style="width:50%;margin:auto;">
                 <h4 style="color:#333;">Anthem Bunker</h4>
                 <p>AV Bunker provides safe, secure services for your most valued possessions.<br></p><a class="btn btn-primary regBtn" role="button" href="https://anthembunker.com/register" target="_blank" style="background-color:#f9a825;border: none;"><i class="fa fa-user-plus"></i> Register</a></div>
             </div>
@@ -161,8 +168,14 @@
 </template>
 
 <script>
+  import { Carousel, Slide } from 'vue-carousel';
+
   export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+      Carousel,
+      Slide
+    }
   }
 </script>
 
