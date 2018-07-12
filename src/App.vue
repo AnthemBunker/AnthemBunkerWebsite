d6096e6cfe60d44ed3b3b6359fec2aab083f0e72
Ubirajara Marsicano <ubirajara_neto@hotmail.com>
2018-07-12 11:03:24 -0300

    added jquery as dependency, loaded jquery bootstra plugins into main page

--- src/App.vue
+++ src/App.vue
@@ -5,16 +5,17 @@
 </template>
 
 <script>
-import Home from './pages/Home';
-import MainNav from './components/navigation/MainNav';
+  import 'bootstrap/dist/js/bootstrap.bundle.min'
+  import Home from './pages/Home';
+  import MainNav from './components/navigation/MainNav';
 
-export default {
-  name: 'app',
-  components: {
-    Home,
-    MainNav
+  export default {
+    name: 'app',
+    components: {
+      Home,
+      MainNav
+    }
   }
-}
 </script>
 
 <style>
