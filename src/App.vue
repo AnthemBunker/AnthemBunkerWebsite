f5cd72115487999c64823a608921395f4ac1b68c
Ubirajara Marsicano <ubirajara_neto@hotmail.com>
2018-07-18 12:24:37 -0300

    removed unecessary components

--- src/App.vue
+++ src/App.vue
@@ -8,15 +8,9 @@
   import 'bootstrap/dist/js/bootstrap.bundle.min'
   import 'bootstrap/dist/css/bootstrap-reboot.min.css'
   import 'bootstrap/dist/css/bootstrap.min.css'
-  import Home from './pages/Home';
-  import MainNav from './components/navigation/MainNav';
 
   export default {
-    name: 'app',
-    components: {
-      Home,
-      MainNav
-    }
+    name: 'app'
   }
 </script>
 
