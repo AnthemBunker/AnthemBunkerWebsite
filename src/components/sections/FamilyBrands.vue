85d8573fb2559f856bfc8ed7ef1903864fffb2c6
daxdax89 <daxdax89@msn.com>
2019-02-21 22:15:50 +0100

    Updates

--- src/components/sections/FamilyBrands.vue
+++ src/components/sections/FamilyBrands.vue
@@ -9,28 +9,95 @@
         </div>
         <div class="row" style="margin-top: 3%;">
           <div class="col-md-6">
-            <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../../assets/img/AV-logo-color.png" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/AV-logo-color.png"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">AnthemVault</h4>
-              <p>Buy, store, sell and gift metals. Gold and Silver ownership made easy, secure and affordable<br></p><a class="btn btn-primary" role="button" href="https://www.anthemvault.com/" target="_blank">Visit AnthemVault</a></div>
+              <p>Buy, store, sell and gift metals. Gold and Silver ownership made easy, secure and affordable
+                <br>
+              </p>
+              <a
+                class="btn btn-primary"
+                role="button"
+                href="https://www.anthemvault.com/"
+                target="_blank"
+              >Visit AnthemVault</a>
+            </div>
           </div>
           <div class="col-md-6">
-            <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height:300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../../assets/img/amagi-metals-logo.gif" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="text-align:center;background-color:white;border-radius:5px;padding:10%;height:300px;margin-bottom: 2%;margin-top: 5%;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/amagi-metals-logo.gif"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">Amagi Metals</h4>
-              <p>Amagi Metals is a global bullion dealer that serves the world with quality precious metals.<br></p><a class="btn btn-primary" role="button" href="https://www.amagimetals.com/" target="_blank">Visit Amagi Metals</a></div>
+              <p>Amagi Metals is a global bullion dealer that serves the world with quality precious metals.
+                <br>
+              </p>
+              <a
+                class="btn btn-primary"
+                role="button"
+                href="https://www.amagimetals.com/"
+                target="_blank"
+              >Visit Amagi Metals</a>
+            </div>
           </div>
         </div>
       </div>
       <div class="container">
         <div class="row">
           <div class="col-md-6">
-            <div class="senka" style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"><img class="img-fluid" src="../../assets/img/Anthem-gold-1.png" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="text-align:center;background-color:white;border-radius:5px;padding:10%;height: 300px;margin-bottom: 2%;margin-top: 5%;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/Anthem-gold-1.png"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">Anthem Gold</h4>
-              <p>Anthem Gold is a global gold dealer that serves the world with quality gold bars that can be obtained with crypto currencies.<br></p><a class="btn btn-primary" role="button" href="https://www.anthemgold.com/" target="_blank">Visit Anthem Gold</a></div>
+              <p>Anthem Gold is a global gold dealer that serves the world with quality gold bars that can be obtained with crypto currencies.
+                <br>
+              </p>
+              <a
+                class="btn btn-primary"
+                role="button"
+                href="https://www.anthemgold.com/"
+                target="_blank"
+              >Visit Anthem Gold</a>
+            </div>
           </div>
           <div class="col-md-6">
-            <div class="senka" style="margin-bottom:2%;margin-top:5%;margin-left:0;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="../../assets/img/AV-Pro-logo-light.png" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="margin-bottom:2%;margin-top:5%;margin-left:0;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/AV-Pro-logo-light.png"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">AnthemVault Pro</h4>
-              <p>Anthem Vault Professional allows investment advisor portfolios to shine with physical Gold &amp; Silver bullion ownership<br></p><a class="btn btn-primary" role="button" href="http://www.anthemvaultprofessional.com/" target="_blank">Visit AnthemVault Pro</a>
+              <p>Anthem Vault Professional allows investment advisor portfolios to shine with physical Gold &amp; Silver bullion ownership
+                <br>
+              </p>
+              <a
+                class="btn btn-primary"
+                role="button"
+                href="http://www.anthemvaultprofessional.com/"
+                target="_blank"
+              >Visit AnthemVault Pro</a>
             </div>
           </div>
         </div>
@@ -40,25 +107,56 @@
       <div class="container">
         <div class="row">
           <div class="col-md-6">
-            <div class="senka" style="margin-right:0;margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"><img class="img-fluid" src="../../assets/img/ANR-logo-web-light.png" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="margin-right:0;margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/ANR-logo-web-light.png"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">Anthem National Reserve</h4>
-              <p>Expert consultation services in the acquisition of bullion precious metals and rare coin investments.<br></p><a class="btn btn-primary" role="button" href="http://www.anthemnationalreserve.com/" target="_blank">Visit ANR</a></div>
+              <p>Expert consultation services in the acquisition of bullion precious metals and rare coin investments.
+                <br>
+              </p>
+              <a
+                class="btn btn-primary"
+                role="button"
+                href="http://www.anthemnationalreserve.com/"
+                target="_blank"
+              >Visit ANR</a>
+            </div>
           </div>
           <div class="col-md-6">
-            <div class="senka" style="margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;margin-bottom: 2%;"><img class="img-fluid" src="../../assets/img/anthem-bunker-logo-light.png" style="width:50%;margin:auto;">
+            <div
+              class="senka"
+              style="margin-top:5%;text-align:center;background-color:white;border-radius:5px;padding:5%;height: 300px;margin-bottom: 2%;"
+            >
+              <img
+                class="img-fluid"
+                src="../../assets/img/anthem-bunker-logo-light.png"
+                style="width:50%;margin:auto;"
+              >
               <h4 style="color:#333;">Anthem Bunker</h4>
-              <p>AV Bunker provides safe, secure services for your most valued possessions.<br></p><a class="btn btn-primary regBtn" role="button" href="https://anthembunker.com/register" target="_blank" style="background-color:#f9a825;border: none;"><i class="fa fa-user-plus"></i> Register</a></div>
+              <p>AV Bunker provides safe, secure services for your most valued possessions.
+                <br>
+              </p>
+              <a
+                class="btn btn-primary regBtn"
+                role="button"
+                href="https://anthembunker.com/register"
+                target="_blank"
+                style="background-color:#f9a825;border: none;"
+              >
+                <i class="fa fa-user-plus"></i> Register
+              </a>
+            </div>
           </div>
         </div>
       </div>
     </div>
-    <div>
-    <video width="320" height="240" controls>
-  <source src="http://207.70.170.171:8080" type="video/mp4">
-      <source src="http://207.70.170.171:8080" type="video/ogg">
-Your browser does not support the video tag.
-</video>
-  </div>
+    <div></div>
   </div>
 </template>
 
